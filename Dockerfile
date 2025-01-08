edc3bf40c2a3e86236ed8334ede08c70065d706b
Diego <diego@celestia.org>
2025-01-08 17:46:21 -0500

    Celestia DA
    - Fix anytrust fallback issues
    - Add `Type()` to dapWritter to differentiate which dapWritter is being used (`celestia` or `anytrust`)
    - pin contracts branch and nitro testnode branch
    
    Update .gitmodules
    
    Update download-machine.sh
    
    Update Dockerfile
    
    Fix interface methods
    
    Merge branch 'celestia-v3.1.2' into celestia-v3.2.1
    
    Pin contracts and testnode

--- Dockerfile
+++ Dockerfile
@@ -45,10 +45,12 @@ WORKDIR /workspace
 RUN apt-get update && apt-get install -y curl build-essential=12.9
 
 FROM wasm-base AS wasm-libs-builder
-	# clang / lld used by soft-float wasm
+# clang / lld used by soft-float wasm
 RUN apt-get update && \
     apt-get install -y clang=1:14.0-55.7~deb12u1 lld=1:14.0-55.7~deb12u1 wabt
-    # pinned rust 1.84.1
+# pinned rust 1.80.1
+RUN curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain 1.80.1 --target x86_64-unknown-linux-gnu,wasm32-unknown-unknown,wasm32-wasi
+# pinned rust 1.84.1
 RUN curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain 1.84.1 --target x86_64-unknown-linux-gnu,wasm32-unknown-unknown,wasm32-wasip1
 COPY ./Makefile ./
 COPY arbitrator/Cargo.* arbitrator/
@@ -82,6 +84,7 @@ COPY ./blsSignatures ./blsSignatures
 COPY ./cmd/chaininfo ./cmd/chaininfo
 COPY ./cmd/replay ./cmd/replay
 COPY ./das/dastree ./das/dastree
+COPY ./das/celestia ./das/celestia
 COPY ./precompiles ./precompiles
 COPY ./statetransfer ./statetransfer
 COPY ./util ./util
@@ -98,6 +101,7 @@ COPY ./go-ethereum ./go-ethereum
 COPY scripts/remove_reference_types.sh scripts/
 COPY --from=brotli-wasm-export / target/
 COPY --from=contracts-builder workspace/contracts/build/contracts/src/precompiles/ contracts/build/contracts/src/precompiles/
+COPY --from=contracts-builder workspace/contracts/build/contracts/src/celestia/ contracts/build/contracts/src/celestia/
 COPY --from=contracts-builder workspace/contracts/node_modules/@offchainlabs/upgrade-executor/build/contracts/src/UpgradeExecutor.sol/UpgradeExecutor.json contracts/
 COPY --from=contracts-builder workspace/contracts-legacy/build/contracts/src/precompiles/ contracts-legacy/build/contracts/src/precompiles/
 COPY --from=contracts-builder workspace/.make/ .make/
@@ -231,11 +235,10 @@ RUN ./download-machine.sh consensus-v31 0x260f5fa5c3176a856893642e149cf128b5a8de
 RUN ./download-machine.sh consensus-v32 0x184884e1eb9fefdc158f6c8ac912bb183bf3cf83f0090317e0bc4ac5860baa39
 RUN ./download-machine.sh consensus-v40-rc.1 0x6dae396b0b7644a2d63b4b22e6452b767aa6a04b6778dadebdd74aa40f40a5c5
 RUN ./download-machine.sh consensus-v40-rc.2 0xa8206be13d53e456c7ab061d94bab5b229d674ac57ffe7281216479a8820fcc0
+RUN ./download-machine.sh v3.2.1-rc.1 0xe81f986823a85105c5fd91bb53b4493d38c0c26652d23f76a7405ac889908287 celestiaorg
 
-FROM golang:1.23.1-bookworm AS node-builder
 WORKDIR /workspace
 ARG version=""
-ARG datetime=""
 ARG modified=""
 ENV NITRO_VERSION=$version
 ENV NITRO_DATETIME=$datetime
--- arbnode/node.go
+++ arbnode/node.go
@@ -36,6 +36,8 @@ import (
 	"github.com/offchainlabs/nitro/broadcaster"
 	"github.com/offchainlabs/nitro/cmd/chaininfo"
 	"github.com/offchainlabs/nitro/das"
+	"github.com/offchainlabs/nitro/das/celestia"
+	celestiaTypes "github.com/offchainlabs/nitro/das/celestia/types"
 	"github.com/offchainlabs/nitro/execution"
 	"github.com/offchainlabs/nitro/execution/gethexec"
 	"github.com/offchainlabs/nitro/solgen/go/bridgegen"
@@ -55,25 +57,26 @@ import (
 )
 
 type Config struct {
-	Sequencer                bool                           `koanf:"sequencer"`
-	ParentChainReader        headerreader.Config            `koanf:"parent-chain-reader" reload:"hot"`
-	InboxReader              InboxReaderConfig              `koanf:"inbox-reader" reload:"hot"`
-	DelayedSequencer         DelayedSequencerConfig         `koanf:"delayed-sequencer" reload:"hot"`
-	BatchPoster              BatchPosterConfig              `koanf:"batch-poster" reload:"hot"`
-	MessagePruner            MessagePrunerConfig            `koanf:"message-pruner" reload:"hot"`
-	BlockValidator           staker.BlockValidatorConfig    `koanf:"block-validator" reload:"hot"`
-	Feed                     broadcastclient.FeedConfig     `koanf:"feed" reload:"hot"`
-	Staker                   legacystaker.L1ValidatorConfig `koanf:"staker" reload:"hot"`
-	Bold                     boldstaker.BoldConfig          `koanf:"bold"`
-	SeqCoordinator           SeqCoordinatorConfig           `koanf:"seq-coordinator"`
-	DataAvailability         das.DataAvailabilityConfig     `koanf:"data-availability"`
-	SyncMonitor              SyncMonitorConfig              `koanf:"sync-monitor"`
-	Dangerous                DangerousConfig                `koanf:"dangerous"`
-	TransactionStreamer      TransactionStreamerConfig      `koanf:"transaction-streamer" reload:"hot"`
-	Maintenance              MaintenanceConfig              `koanf:"maintenance" reload:"hot"`
-	ResourceMgmt             resourcemanager.Config         `koanf:"resource-mgmt" reload:"hot"`
-	BlockMetadataFetcher     BlockMetadataFetcherConfig     `koanf:"block-metadata-fetcher" reload:"hot"`
-	ConsensusExecutionSyncer ConsensusExecutionSyncerConfig `koanf:"consensus-execution-syncer"`
+	Sequencer            bool                           `koanf:"sequencer"`
+	ParentChainReader    headerreader.Config            `koanf:"parent-chain-reader" reload:"hot"`
+	InboxReader          InboxReaderConfig              `koanf:"inbox-reader" reload:"hot"`
+	DelayedSequencer     DelayedSequencerConfig         `koanf:"delayed-sequencer" reload:"hot"`
+	BatchPoster          BatchPosterConfig              `koanf:"batch-poster" reload:"hot"`
+	MessagePruner        MessagePrunerConfig            `koanf:"message-pruner" reload:"hot"`
+	BlockValidator       staker.BlockValidatorConfig    `koanf:"block-validator" reload:"hot"`
+	Feed                 broadcastclient.FeedConfig     `koanf:"feed" reload:"hot"`
+	Staker               legacystaker.L1ValidatorConfig `koanf:"staker" reload:"hot"`
+	Bold                 boldstaker.BoldConfig          `koanf:"bold"`
+	SeqCoordinator       SeqCoordinatorConfig           `koanf:"seq-coordinator"`
+	DataAvailability     das.DataAvailabilityConfig     `koanf:"data-availability"`
+	SyncMonitor          SyncMonitorConfig              `koanf:"sync-monitor"`
+	Dangerous            DangerousConfig                `koanf:"dangerous"`
+	TransactionStreamer  TransactionStreamerConfig      `koanf:"transaction-streamer" reload:"hot"`
+	Maintenance          MaintenanceConfig              `koanf:"maintenance" reload:"hot"`
+	ResourceMgmt         resourcemanager.Config         `koanf:"resource-mgmt" reload:"hot"`
+	BlockMetadataFetcher BlockMetadataFetcherConfig     `koanf:"block-metadata-fetcher" reload:"hot"`
+	Celestia             celestia.CelestiaConfig        `koanf:"celestia-cfg"`
+	DAPreference         []string                       `koanf:"da-preference"`
 	// SnapSyncConfig is only used for testing purposes, these should not be configured in production.
 	SnapSyncTest SnapSyncConfig
 }
@@ -144,7 +147,7 @@ func ConfigAddOptions(prefix string, f *flag.FlagSet, feedInputEnable bool, feed
 	TransactionStreamerConfigAddOptions(prefix+".transaction-streamer", f)
 	MaintenanceConfigAddOptions(prefix+".maintenance", f)
 	BlockMetadataFetcherConfigAddOptions(prefix+".block-metadata-fetcher", f)
-	ConsensusExecutionSyncerConfigAddOptions(prefix+".consensus-execution-syncer", f)
+	celestia.CelestiaDAConfigAddOptions(prefix+".celestia-cfg", f)
 }
 
 var ConfigDefault = Config{
@@ -560,6 +563,8 @@ func getDAS(
 	var daReader das.DataAvailabilityServiceReader
 	var dasLifecycleManager *das.LifecycleManager
 	var dasKeysetFetcher *das.KeysetFetcher
+	var celestiaReader celestiaTypes.CelestiaReader
+	var celestiaWriter celestiaTypes.CelestiaWriter
 	if config.DataAvailability.Enable {
 		var err error
 		if config.BatchPoster.Enable {
@@ -586,6 +591,16 @@ func getDAS(
 		return nil, nil, nil, errors.New("a data availability service is required for this chain, but it was not configured")
 	}
 
+	if config.Celestia.Enable {
+		celestiaService, err := celestia.NewCelestiaDASRPCClient(config.Celestia.URL)
+		if err != nil {
+			return nil, err
+		}
+
+		celestiaReader = celestiaService
+		celestiaWriter = celestiaService
+	}
+
 	// We support a nil txStreamer for the pruning code
 	if txStreamer != nil && txStreamer.chainConfig.ArbitrumChainParams.DataAvailabilityCommittee && daReader == nil {
 		return nil, nil, nil, errors.New("data availability service required but unconfigured")
@@ -597,24 +612,9 @@ func getDAS(
 	if blobReader != nil {
 		dapReaders = append(dapReaders, daprovider.NewReaderForBlobReader(blobReader))
 	}
-
-	return daWriter, dasLifecycleManager, dapReaders, nil
-}
-
-func getInboxTrackerAndReader(
-	ctx context.Context,
-	arbDb ethdb.Database,
-	txStreamer *TransactionStreamer,
-	dapReaders []daprovider.Reader,
-	config *Config,
-	configFetcher ConfigFetcher,
-	l1client *ethclient.Client,
-	l1Reader *headerreader.HeaderReader,
-	deployInfo *chaininfo.RollupAddresses,
-	delayedBridge *DelayedBridge,
-	sequencerInbox *SequencerInbox,
-	exec execution.ExecutionSequencer,
-) (*InboxTracker, *InboxReader, error) {
+	if celestiaReader != nil {
+		dapReaders = append(dapReaders, celestiaTypes.NewReaderForCelestia(celestiaReader))
+	}
 	inboxTracker, err := NewInboxTracker(arbDb, txStreamer, dapReaders, config.SnapSyncTest)
 	if err != nil {
 		return nil, nil, err
@@ -878,9 +878,29 @@ func getBatchPoster(
 		if txOptsBatchPoster == nil && config.BatchPoster.DataPoster.ExternalSigner.URL == "" {
 			return nil, errors.New("batchposter, but no TxOpts")
 		}
-		var dapWriter daprovider.Writer
-		if daWriter != nil {
-			dapWriter = daprovider.NewWriterForDAS(daWriter)
+		dapWriters := []daprovider.Writer{}
+		for _, providerName := range config.DAPreference {
+			nilWriter := false
+			switch strings.ToLower(providerName) {
+			case "anytrust":
+				log.Info("Adding DapWriter", "type", "anytrust")
+				if daWriter != nil {
+					dapWriters = append(dapWriters, daprovider.NewWriterForDAS(daWriter))
+				} else {
+					nilWriter = true
+				}
+			case "celestia":
+				log.Info("Adding DapWriter", "type", "celestia")
+				if celestiaWriter != nil {
+					dapWriters = append(dapWriters, celestiaTypes.NewWriterForCelestia(celestiaWriter))
+				} else {
+					nilWriter = true
+				}
+			}
+
+			if nilWriter {
+				log.Error("encountered nil daWriter", "daWriter", providerName)
+			}
 		}
 		var err error
 		batchPoster, err = NewBatchPoster(ctx, &BatchPosterOpts{
@@ -893,7 +913,7 @@ func getBatchPoster(
 			Config:        func() *BatchPosterConfig { return &configFetcher.Get().BatchPoster },
 			DeployInfo:    deployInfo,
 			TransactOpts:  txOptsBatchPoster,
-			DAPWriter:     dapWriter,
+			DAPWriters:    dapWriters,
 			ParentChainID: parentChainID,
 			DAPReaders:    dapReaders,
 		})
